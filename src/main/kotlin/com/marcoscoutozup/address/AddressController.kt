e30051a8804853da8fc54f00a86c1510b916e593
Marcos Couto <marcosmartinellicouto@gmail.com>
2020-12-30 17:56:37 -0300

    Inserindo tracing com Jaeger

--- src/main/kotlin/com/marcoscoutozup/address/AddressController.kt
+++ src/main/kotlin/com/marcoscoutozup/address/AddressController.kt
@@ -5,15 +5,20 @@ import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
+import io.micronaut.tracing.annotation.ContinueSpan
+import io.micronaut.tracing.annotation.SpanTag
+import io.opentracing.Tracer
 
 @Controller("/address")
-class AddressController(val client: AddressClient) {
+open class AddressController(val client: AddressClient, val tracer: Tracer) {
 
+    @ContinueSpan
     @Get("/{cep}")
-    fun getAddress(@PathVariable cep: String): HttpResponse<*> {
+    open fun getAddress(@SpanTag("cep") @PathVariable cep: String): HttpResponse<*> {
         val address = client.getAddress(cep)
 
         if(address.status != HttpStatus.OK){
+            tracer.activeSpan().log("Address not found ")
             return HttpResponse.notFound(mapOf("message" to "Cep não encontrado"))
         }
 
